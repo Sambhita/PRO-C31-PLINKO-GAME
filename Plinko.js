2b28a4eae94031eeab5b5a13df14e3467898d96a
Sambhita <77112921+Sambhita@users.noreply.github.com>
2021-03-27 13:07:21 +0530

    Add files via upload

--- Plinko.js
+++ Plinko.js
@@ -2,14 +2,13 @@ class Plinko{
     constructor(x,y){
         var options = {
             isStatic:true,
-            //restitution: 1,
+            restitution: 1,
             //friction: 0
         }
         this.x = x;
         this.y = y;
         this.r = 10;
         this.body = Bodies.circle(x,y,10 ,options);
-        this.image = loadImage("images/circle.png");
         World.add(world,this.body);
 
     }
